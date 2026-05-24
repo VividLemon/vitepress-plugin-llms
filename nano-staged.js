4b0d7e52c8a92bfcc276bfdedba9ff46c2200b33
Isa <hi@okinea.dev>
2026-05-24 09:39:18 +0200

    refactor: migrate from lint-staged` to `nano-staged`
    https://e18e.dev/docs/replacements/lint-staged.html

--- nano-staged.js
+++ nano-staged.js
@@ -1,6 +1,6 @@
-import type { Configuration } from 'lint-staged'
-
-const config: Configuration = {
+// oxlint-disable-next-line jsdoc/check-tag-names
+/** @type {import('nano-staged').Configuration} */
+const config = {
 	'*': ['bun run format --no-error-on-unmatched-pattern', 'cspell --no-error-on-empty'],
 	'*.{ts,vue}': () => ['bun run lint'],
 }
